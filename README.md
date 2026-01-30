0f726490250ad2ed89f45d8351e2f8fc5db2fbed
schweyuanzig <2012source@gmail.com>
2026-01-30 21:33:04 +0300

    v1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 3b1m.org
+3blocks1modpack is an anarchy server founded in February 2023, with a world file that hasn't been reset since July 2023.
--- index.js
+++
@@ -1,8 +0,0 @@
-const resimler = [
-            './storage/img/1.png',
-            './storage/img/2.png',
-            './storage/img/3.png',
-            './storage/img/4.png'
-        ];
-const rastgeleIndex = Math.floor(Math.random() * resimler.length);
-document.body.style.backgroundImage = `url('${resimler[rastgeleIndex]}')`;
